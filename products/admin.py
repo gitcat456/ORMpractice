886bd8b14e79698ec3b25ceca0530eec9ad22de5
gitcat456 <okothdenzel65@gmail.com>
2025-11-08 00:53:13 +0300

    customizes the dashboard for a better admin experience

--- products/admin.py
+++ products/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import Product # model import 
 
 #simple registration
-admin.site.register(Product)
+#admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category', 'description') # columns in list view
+    list_filter = ('category',)  #filter sidebar
+    search_fields = ('name', 'description')  #search bar
+    ordering = ('price',)  #default ordering
+
+
+admin.site.register(Product, ProductAdmin)
