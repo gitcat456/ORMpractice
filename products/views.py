7ab1a6e76342c4f210b4bfde685a2c2eba47b762
gitcat456 <okothdenzel65@gmail.com>
2025-11-08 23:33:38 +0300

    make sure to add the template files to view the product list

--- products/views.py
+++ products/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 def home_page(request):
     return HttpResponse("Welcome to ERSD E-commercer store ")
+
+
+def products_list(request):
+    """Retrieves all books and renders a template displaying the list"""
+    products = Product.objects.all()  # Fetch all product instances from the database
+    context = {'product_list': products}  # Create a context dictionary with product list
+    return render(request, 'products/product_list.html', context)
