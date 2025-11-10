fae24227866a41d61bacdaacca282e33efb6a12f
gitcat456 <okothdenzel65@gmail.com>
2025-11-10 13:33:08 +0300

    objects.all() returns a queryset which is a list like collection of model instances or rathermodel objects

--- products/views.py
+++ products/views.py
@@ -3,14 +3,18 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 from .models import Product
 
+#function based view
 def home_page(request):
     return HttpResponse("Welcome to ERSD E-commercer store ")
 
 
 def products_list(request):
     """Retrieves all books and renders a template displaying the list"""
-    products = Product.objects.all()  # Fetch all product instances from the database
+    products = Product.objects.all()  # Fetch all product instances from the database 
+    #returns a Django QuerySet — which behaves like a list of your model objects.
+    #Think of a QuerySet as a list-like collection of model instances
     context = {'product_list': products}  # Create a context dictionary with product list
+    print(context)
     return render(request, 'products/product_list.html', context)
 
 class HelloView(TemplateView):
