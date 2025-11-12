445c7494587781672ae9fb7cfab36d1db1b4a5b0
gitcat456 <okothdenzel65@gmail.com>
2025-11-12 23:12:28 +0300

    inherits ListView to display a list of all product instances in the db

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
-from .models import Category
+from django.views.generic import TemplateView, ListView
+from .models import Category, Product
 
 #function based view
 #def home_page(request):
@@ -10,14 +10,20 @@ from .models import Category
 class HomePageView(TemplateView):
     """A class based view rendering a template named hello.html"""
     template_name = 'products/home.html'
-
-
+    
+    
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
+    
+    
 def category_list(request):
-    """Retrieves all books and renders a template displaying the list"""
+    """Retrieves all categories and renders a template displaying the list"""
     categories = Category.objects.all()  # Fetch all category instances from the database 
     #returns a Django QuerySet — which behaves like a list of your model objects.
     #Think of a QuerySet as a list-like collection of model instances
-    context = {'catefgory_list': categories}  # Create a context dictionary with product list
+    context = {'category_list': categories}  # Create a context dictionary with product list
     print(context)
     return render(request, 'products/product_desc.html', context)
 
