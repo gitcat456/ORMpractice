56689f803d8661193d77cf4da8bdf6451665b613
gitcat456 <okothdenzel65@gmail.com>
2025-11-12 10:01:33 +0300

    refactors to match my new model

--- products/admin.py
+++ products/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-from .models import Product # model import 
+from .models import Category # model import 
 
 #simple registration
 #admin.site.register(Product)
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category', 'description') # columns in list view
-    list_filter = ('category',)  #filter sidebar
-    search_fields = ('name', 'description')  #search bar
-    ordering = ('price',)  #default ordering
+    list_display = ('id', 'name') # columns in list view
+    list_filter = ('id',)  #filter sidebar
+    search_fields = ('name', 'id')  #search bar
+   # ordering = ('price',)  #default ordering
 
 
-admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, ProductAdmin)
