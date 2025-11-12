d23be42962b1ff4e93a373c7b2aef5e085cabfec
gitcat456 <okothdenzel65@gmail.com>
2025-11-12 10:02:19 +0300

    refactors to match my new Category model

--- products/views.py
+++ products/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from .models import Product
+from .models import Category
 
 #function based view
 def home_page(request):
     return HttpResponse("Welcome to ERSD E-commercer store ")
 
 
-def products_list(request):
+def category_list(request):
     """Retrieves all books and renders a template displaying the list"""
-    products = Product.objects.all()  # Fetch all product instances from the database 
+    categories = Category.objects.all()  # Fetch all category instances from the database 
     #returns a Django QuerySet — which behaves like a list of your model objects.
     #Think of a QuerySet as a list-like collection of model instances
-    context = {'product_list': products}  # Create a context dictionary with product list
+    context = {'catefgory_list': categories}  # Create a context dictionary with product list
     print(context)
-    return render(request, 'products/product_list.html', context)
+    return render(request, 'products/product_desc.html', context)
+
 
 class HelloView(TemplateView):
     """A class based view rendering a template named hello.html"""
