b1688a37350acf4b47b4719ab931be823fd2794d
gitcat456 <okothdenzel65@gmail.com>
2025-11-12 11:43:50 +0300

    adds price field

--- products/models.py
+++ products/models.py
@@ -8,6 +8,7 @@ class Category(models.Model):
     
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     category = models.ForeignKey(
         Category,
         on_delete=models.PROTECT,
@@ -16,3 +17,5 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+    
+
