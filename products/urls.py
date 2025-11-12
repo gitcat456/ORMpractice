97718bd05d5c2fb42b6e878cd3919ccef03cabbc
gitcat456 <okothdenzel65@gmail.com>
2025-11-12 23:13:00 +0300

    url reconfiguration

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path 
-from .views import HomePageView
+from .views import ProductListView, HomePageView
 from . import views
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('categories/', views.category_list, name = 'category_list'),
-    #path('classproducts/', ProductListView.as_view(), name ="classbased_view"),
+    path('product_list/', ProductListView.as_view(), name = 'product_list')
 ]
