a6701bfed4c9053b20644ef4e96deb962c5b5802
gitcat456 <okothdenzel65@gmail.com>
2025-11-13 08:06:54 +0300

    adds deletion url

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,9 @@ from .views import(
     ProductListView,
     HomePageView, 
     ProductCreateView,
-    ProductDetailView
+    ProductDetailView,
+    ProductUpdateView,
+    ProductDeleteView
     ) 
 from . import views
 
@@ -12,5 +14,8 @@ urlpatterns = [
     path('categories/', views.category_list, name = 'category_list'),
     path('products/', ProductListView.as_view(), name = 'product_list'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
-    path('products/new/', ProductCreateView.as_view(), name='product-create')
+    path('products/new/', ProductCreateView.as_view(), name='product-create'),
+    path('products/<int:pk>/edit', ProductUpdateView.as_view(), name='product-update'),
+    path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product-delete'),
+  
 ]
