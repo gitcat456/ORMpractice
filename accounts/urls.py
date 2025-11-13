412b5defb87e46c3e36702ed77ad33a5d6c0a1c3
gitcat456 <okothdenzel65@gmail.com>
2025-11-13 09:27:00 +0300

    setsup urlpatterns for authentication routes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 #from django.contrib.auth import views as auth_views  then u auth_views.LoginView
+from .views import SignUpView
 from django.contrib.auth.views import (
     LoginView,
     LogoutView,
@@ -8,8 +9,9 @@ from django.contrib.auth.views import (
 )
 
 urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
-    path('password_change/', PasswordChangeView.as_view(template_name='password_change.html'), name='password_change'),
+    path('password_change/', PasswordChangeView.as_view(template_name='accounts/password_change.html'), name='password_change'),
     path('password_change/done/', PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'), name='password_change_done')
 ]
