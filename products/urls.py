42c2f8c64282ce6196f1aa3d5b712bea0b06b88b
gitcat456 <okothdenzel65@gmail.com>
2025-11-13 07:31:19 +0300

    <int:pk>capture the next part of the url as an integer and pass it as a variable pk

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
-from .views import ProductListView, HomePageView
+from .views import ProductListView, HomePageView, ProductDetailView
 from . import views
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('categories/', views.category_list, name = 'category_list'),
-    path('product_list/', ProductListView.as_view(), name = 'product_list')
+    path('product_list/', ProductListView.as_view(), name = 'product_list'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail')
 ]
