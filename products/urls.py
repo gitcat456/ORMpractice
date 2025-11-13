854c5ec0a7c85cb9bce738288c5d9afb53294247
gitcat456 <okothdenzel65@gmail.com>
2025-11-13 07:49:46 +0300

    creates the route to the new product form

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path 
-from .views import ProductListView, HomePageView, ProductDetailView
+from .views import(
+    ProductListView,
+    HomePageView, 
+    ProductCreateView,
+    ProductDetailView
+    ) 
 from . import views
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('categories/', views.category_list, name = 'category_list'),
-    path('product_list/', ProductListView.as_view(), name = 'product_list'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail')
+    path('products/', ProductListView.as_view(), name = 'product_list'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('products/new/', ProductCreateView.as_view(), name='product-create')
 ]
