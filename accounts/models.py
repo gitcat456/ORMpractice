0a31b999007b04566bdc2d20678ca52c55729a95
gitcat456 <okothdenzel65@gmail.com>
2025-11-16 18:48:13 +0300

    adds phone_number field to the custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    phone_number = models.CharField(max_length=12, null=True, default='07********')
+    
     ROLE_CHOICES = (
      # ('database_value', 'Human_readable_name')
         ('admin', 'Admin'),
