a6421a2ed47e8d8603f5d211c2cc25ce3958f2d3
gitcat456 <okothdenzel65@gmail.com>
2025-11-16 13:11:50 +0300

    extends the user model to add role field

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+     # ('database_value', 'Human_readable_name')
+        ('admin', 'Admin'),
+        ('customer', 'Customer'),
+    )
+    
+    role = models.CharField(
+        max_length=20, 
+        choices=ROLE_CHOICES,
+        default='customer'
+    )
+    
+    def is_admin(self):
+        return self.role == 'admin'
+    
+    def is_customer(self):
+        return self.role == 'customer'
