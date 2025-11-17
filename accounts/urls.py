87d452c14d32b48aa3b71a4a122d346751223344
gitcat456 <okothdenzel65@gmail.com>
2025-11-17 05:01:09 +0300

    imports custom login class from views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 #from django.contrib.auth import views as auth_views  then u auth_views.LoginView
-from .views import SignUpView
+from .views import SignUpView, CustomLoginView
 from django.contrib.auth.views import (
-    LoginView,
     LogoutView,
     PasswordChangeView,
     PasswordChangeDoneView
@@ -10,7 +9,7 @@ from django.contrib.auth.views import (
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('login/', LoginView.as_view(template_name='accounts/login.html', redirect_authenticated_user = True), name='login'),
+    path('login/', CustomLoginView.as_view(redirect_authenticated_user = True), name='login'),
     path('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
     path('password_change/', PasswordChangeView.as_view(template_name='accounts/password_change.html'), name='password_change'),
     path('password_change/done/', PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'), name='password_change_done')
