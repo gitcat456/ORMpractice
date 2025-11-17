df0101b2dbe9344ce5f933fe4658a051346ce971
gitcat456 <okothdenzel65@gmail.com>
2025-11-17 05:00:37 +0300

    creates CustomLoginView to use the custom form

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,16 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .models import User
-from accounts.forms import CustomUserCreationForm
+from accounts.forms import CustomUserCreationForm, CustomLoginForm
+from django.contrib.auth.views import LoginView
+
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+    
+class CustomLoginView(LoginView):
+    form_class = CustomLoginForm
+    template_name = 'accounts/login.html'
+    
